0a48861df982e62549a060f1f90a8e8e9278a61c
Edgar Cortez <eac0001@dcccd.edu>
2023-07-21 11:43:51 -0500

    setting up the structure to do the user authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthController } from './user/auth/auth.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UserModule],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
