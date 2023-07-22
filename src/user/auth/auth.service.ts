f554269f261ab88b05996c509dbbce3a8a9db315
Edgar Cortez <eac0001@dcccd.edu>
2023-07-22 14:44:35 -0500

    successfully hashed the password using bcryptjs

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
 
 interface SignupParams {
   name: string;
@@ -16,18 +17,21 @@ export class AuthService {
   //   return 'signed up';
   // }
 
-  async signup({ email }: SignupParams) {
+  async signup({ email, password }: SignupParams) {
     // const userExists = await this.prismaService.user.findUnique({ // the email is complaining that it needs to be paired with id or another property
     const userExists = await this.prismaService.user.findFirst({
       where: {
         email,
       },
     });
-
     console.log({ userExists });
 
     if (userExists) {
       throw new ConflictException();
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log({ hashedPassword });
   }
+  //4:48:58
 }
